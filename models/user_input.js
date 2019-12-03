ed866338d169574f6bdcfbf1ba5668a270b19307
herbstar14 <54959136+herbstar14@users.noreply.github.com>
2019-12-03 14:39:25 -0600

    Add files via upload

--- models/user_input.js
+++ models/user_input.js
@@ -1,1 +1,13 @@
-const mongoose = require()
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+var user_input_Schema = new Schema({
+    title:String,
+    description:String,
+    pubished_data:{
+        type:Date,
+        default:Date.now
+    }
+
+})
+module.exports = mongoose.model('user_input', user_input_Schema)
