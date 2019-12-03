17c69d1e86b8ecd60f26797a09eede3319f3e82f
herbstar14 <54959136+herbstar14@users.noreply.github.com>
2019-12-03 15:00:34 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser') 
 const ejs = require('ejs')
+//they added a lot of stuff in this index file 
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const mongoose = require('mongoose')
+const connectDB = require('./modules/connectMongoDB')
+connectDB();
+
+//they are fileStore and session
+
+app.use(session({
+    secret: "andrew",//security key
+    resave: false,
+    saveUninitialized: false,
+    store: FileStore(),
+
+
+}))
+
 
 app.use(express.static('public'))
 app.set('views' , __dirname + '/views')
